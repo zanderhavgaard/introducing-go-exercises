bd94b04e6106543fb57e29edf44555f2a40d6d83
zanderhavgaard <zander@havgaard.dk>
2021-10-18 22:36:12 +0200

    finsihed chapter 7

--- chapter_7/main.go
+++ chapter_7/main.go
@@ -36,6 +36,78 @@ func circleArea3(c *Circle) float64 {
 	return math.Pi * c.r * c.r
 }
 
+// method, that only works on Circle structs
+// the (c *Circle) is called the 'reciever' in a method signature
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+// define type for rectangles
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+// define method for rectangle struct to calculate area
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
+// define person type
+type Person struct {
+	Name string
+}
+
+// method on Person struct
+func (p *Person) Talk() {
+	fmt.Println("Hi, my name is", p.Name)
+}
+
+// struct with embedded type, an Android is a Person
+// sometimes also called an 'anonymous filed'
+type Android struct {
+	Person
+	Model string
+}
+
+// create interface for shapes, structs must then implement the interface,
+// in order to be able to use the interface methods
+type Shape interface {
+	area() float64
+}
+
+// create interface method for the shape interface
+func totalArea(shapes ...Shape) float64 {
+	var area float64
+	for _, s := range shapes {
+		area += s.area()
+	}
+	return area
+}
+
+// alternate implicit syntax
+func totalArea2(shapes ...Shape) (area float64) {
+	for _, s := range shapes {
+		area += s.area()
+	}
+	return
+}
+
+// a struct that uses an interface
+type MultiShape struct {
+	Shapes []Shape
+}
+
+// implement the area() method for the MultiShape struct,
+// which makes the MultiShape a Shape, as it implements the interface
+func (m *MultiShape) area() (area float64) {
+	for _, s := range m.Shapes {
+		area += s.area()
+	}
+	return
+}
+
 func main() {
 	var rx1, ry1 float64 = 0, 0
 	var rx2, ry2 float64 = 10, 10
@@ -78,4 +150,42 @@ func main() {
 	fmt.Println(circleArea2(c))
 	// call funciton using pointer to struct
 	fmt.Println(circleArea3(&c))
+
+	area := c.area()
+	fmt.Println(area)
+
+	rec := Rectangle{0, 0, 10, 10}
+	area2 := rec.area()
+	fmt.Println(area2)
+
+	person := Person{"foo"}
+	person.Talk()
+
+	robot := new(Android)
+	fmt.Println(robot)
+	// we can call methods of the embedded Person type
+	robot.Person.Talk()
+	// because Person is embedded we can call them directly
+	robot.Talk()
+
+	// declare struct variable with embedded type
+	robot2 := Android{Person: Person{Name: "bar"}, Model: "123"}
+	fmt.Println(robot2)
+	robot2.Person.Talk()
+	robot2.Talk()
+
+	// use interface method
+	ta := totalArea(&c, &rec)
+	fmt.Println(ta)
+
+	// declare MultiShape struct using pointer types
+	ms := MultiShape{
+		Shapes: []Shape{
+			&Circle{0, 0, 5},
+			&Rectangle{0, 0, 10, 10},
+		},
+	}
+	area3 := ms.area()
+	fmt.Println(area3)
+	// for how to implement this without pointer types, see the book errata: https://www.oreilly.com/catalog/errata.csp?isbn=0636920046516
 }
